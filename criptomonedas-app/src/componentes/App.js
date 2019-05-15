2cffcac147b9b6dae2785df22f8dbcb9ad2930dc
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-05-15 11:05:12 -0500

    Inicio de la Aplicación

---
+++ criptomonedas-app/src/componentes/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Imagen from './Imagen';
+import './../css/App.css';
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Imagen></Imagen>
+      </div>
+    )
+  }
+}
+
+/*
+function App() {
+  return (
+    
+  );
+}
+*/
+
+export default App;
